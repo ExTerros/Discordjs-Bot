0247957e44daf3189028e992c09c247cb4bb275f
exterros <nicolasdu76250@live.fr>
2022-09-17 20:11:57 +0200

    Add pool commands

---
+++ commands/pool.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder, Guild } = require('discord.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('vote')
+		.setDescription("créer un vote")
+		.addStringOption(option =>
+			option
+				.setName('message')
+				.setDescription('Votre message')
+				.setRequired(true)),
+	async execute(interaction) {
+		const message = interaction.options.getString('message');
+		const poolEmbed = new EmbedBuilder()
+		.setAuthor({ name: "📊 Nouveau Vote"})
+		.setColor('#30C0FF')
+		.setTitle(message)
+		.setDescription('@everyone')
+
+		const vote = await interaction.reply({ embeds: [poolEmbed], fetchReply: true });
+		vote.react('✅');
+		vote.react('🤔');
+		vote.react('❌');
+
+
+	},
+};
