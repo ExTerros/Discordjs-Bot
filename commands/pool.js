007d6e8d1c9fb4fbf7d146347f4cb6742a19e890
exterros <nicolasdu76250@live.fr>
2022-09-25 00:36:08 +0200

    Add name of user who response to the pool

--- commands/pool.js
+++ commands/pool.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder, } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('vote')
+		.setName('votetest')
 		.setDescription("créer un vote")
 		.addStringOption(option =>
 			option
@@ -13,20 +13,57 @@ module.exports = {
 				.setRequired(true)),
 	async execute(interaction) {
 		interaction.channel.send("@everyone");
+		const alreadyPressed = []
+		const whoPoolWhat = []
+
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('✅')
+					.setLabel('Oui ✅')
+					.setStyle(ButtonStyle.Success))
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('🤔')
+					.setLabel('Peut-être 🤔')
+					.setStyle(ButtonStyle.Secondary))
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('❌')
+					.setLabel('Non ❌')
+					.setStyle(ButtonStyle.Danger),
+			);
+
 		const message = interaction.options.getString('message');
 		const poolEmbed = new EmbedBuilder()
 		.setAuthor({ name: "📊 Nouveau Vote"})
 		.setColor('#30C0FF')
 		.setTitle(message)
 
-		const vote = await interaction.reply({ embeds: [poolEmbed], fetchReply: true });
-		vote.react('✅');
-		vote.react('🤔');
-		vote.react('❌');
+		await interaction.reply({ embeds: [poolEmbed], fetchReply: true, components: [row] });
 
+		const collector = interaction.channel.createMessageComponentCollector();
 		
-		
+		collector.on('collect', async i => {
+			if (!!alreadyPressed.find(id => {  
+				return id.ID === i.user.id+i.message.id
+			  })) {
+				i.reply({ content: `Tu à déjà voter ${i.user.username}!`, ephemeral: true })
+			  } else {
+				whoPoolWhat.push({what: `${i.customId}`, who:`**${i.user.username}**`})
 
+				const newEmbed = EmbedBuilder.from(poolEmbed)
+				whoPoolWhat.forEach(pool => {
+					newEmbed.addFields(
+						{ name: pool['what'], value: `**${pool['who']}**`, inline: true},
+					)
+				});
+
+				i.update({ embeds: [newEmbed],  fetchReply: true });
+
+				alreadyPressed.push({ID: i.user.id+i.message.id})
+			}
+		});  
 
 	},
 };
